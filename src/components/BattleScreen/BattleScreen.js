dc4b5387affd82385e2bafcb769e405a9f9a9d8b
Ben Holden <bnhldn@gmail.com>
2023-10-16 16:31:25 -0600

    Add Search functionality

--- src/components/BattleScreen/BattleScreen.js
+++ src/components/BattleScreen/BattleScreen.js
@@ -5,12 +5,12 @@ function BattleScreen({player1, player2, winner}){
     <div className="battle-screen">
       <div className="battle-images"> 
         <div className="selected-fighter1">
-          <img src={player1.image} alt={player1.name} />
+          <img className="battle-image" src={player1.image} alt={player1.name} />
           <p className="fighter1-name">{player1.name}</p>
         </div>
         {winner?<p>{winner}</p>:<p className="bang">💥</p>}
         <div className="selected-fighter2">
-          <img src={player2.image} alt={player2.name} />
+          <img className="battle-image" src={player2.image} alt={player2.name} />
           <p className="fighter2-name">{player2.name}</p>
         </div>
       </div>
--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,9 +1,15 @@
+import { useState } from "react"
 import CharacterCard from "../CharacterCard/CharacterCard"
 import "./Characters.css"
 
-export default function Characters({characters, selectCharacter, player1, player2, displayFight}){
+export default function Characters({setDropDownInput, dropDownInput, filterCharacters, characters, selectCharacter, player1, player2, displayFight, getCharacters}){
+  const [userInput, setUserInput] = useState('')
   let displayedCharacters = characters.map(character=>{
+    console.log(character.publisher===dropDownInput)
+    console.log(character.publisher)
+    console.log(dropDownInput)
     character.key = character.id
+    if(dropDownInput==="all"){
   return (
     <CharacterCard 
       name={character.name}
@@ -13,33 +19,64 @@ export default function Characters({characters, selectCharacter, player1, player
       character={character}
       selectCharacter={selectCharacter}
     />
-  )
+  )} else if(character.publisher===dropDownInput){
+  return (
+    <CharacterCard 
+      name={character.name}
+      image={character.image}
+      id={character.id}
+      key={character.id}
+      character={character}
+      selectCharacter={selectCharacter}
+    /> 
+  )} else if(character.publisher !== 'DC Comics' || 'marvel'){
+  return (
+    <CharacterCard 
+      name={character.name}
+      image={character.image}
+      id={character.id}
+      key={character.id}
+      character={character}
+      selectCharacter={selectCharacter}
+    />
+  )} 
   })
-  console.log(player1.image)
+
 return(
   <div className="characters">
     <div className="characters-header">
       {player1.image !== undefined?
       <div>
         <p>Player 1:</p>
-        <img src={player1.image} alt={player1.name} /> 
+        <img className="character-image-header" src={player1.image} alt={player1.name} /> 
         <p>{player1.name}</p>
-      </div>:<p>Please Choose Player 1!!!</p>}
+      </div>:<p>Please Select Player #1!!!</p>}
       <div className="title-search">
         {player1.image&&player2.image?<button className="fight-button" onClick={()=>{displayFight()}}>Fight!!!</button>
-        :<h1>Choose Your Characters!</h1>} 
-        <input type="text" placeholder="Search Characters"/>
-        <button>Search</button>
+        :<h2>Choose Your Characters!</h2>} 
+        <h3>Choose Publisher:</h3>
+        <select name="publisher-dropdown" id="Select" label="choose" onChange={event =>{ 
+        setDropDownInput(event.target.value)}}>
+          <option value="all">ALL Publishers</option>
+          <option value="DC Comics">DC Comics</option>
+          <option value="marvel">Marvel</option>
+          <option value="other">Other</option>
+        </select>
+        <h3>Search By Name:</h3>
+        <input type="text" placeholder="Search Names" name="searchCharacters" onChange={event =>{ 
+        getCharacters()
+        setUserInput(event.target.value)}}/>
+        <button onClick={()=>filterCharacters(userInput)}>Search</button>
       </div>
       {player2.image !== undefined?
       <div>
         <p>Player 2:</p>
-        <img src={player2.image} alt={player2.name} /> 
+        <img className="character-image-header" src={player2.image} alt={player2.name} /> 
         <p>{player2.name}</p>
-      </div>:<p>Please Choose Player 2!!!</p>}
+      </div>:<p>Please Select Player #2!!!</p>}
     </div>
     <div className="characters-list">
-      {displayedCharacters}
+      {displayedCharacters.length?displayedCharacters:<p>No characters found, please try a different search.</p>}
     </div>
   </div>
 )
