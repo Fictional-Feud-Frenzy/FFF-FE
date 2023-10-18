15689d96768dc8641385338808c37b0a4cf3ffb5
Denzel Turambi <dturambi26@gmail.com>
2023-10-17 18:57:51 -0600

    feat: Add Link to character cards

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function CharacterCard({ name, image, id, character, selectCharacter }) {
- return (
-  <div id={id} className="character-card" onClick={()=>selectCharacter(character)} key={character.id}>
-    <img className="character-images" src={image} alt={name} />
-    <h3 className="name">{name}</h3>
-    </div>
- ) 
+  return (
+    <Link to={`/${id}`}>
+      <div id={id} className="character-card" onClick={() => selectCharacter(character)} key={character.id}>
+        <img className="character-images" src={image} alt={name} />
+        <h3 className="name">{name}</h3>
+      </div>
+    </Link>
+  )
 }
 
 export default CharacterCard;
