e68068022d8df9700289cc776ec472ec528655f0
Denzel Turambi <dturambi26@gmail.com>
2023-10-19 16:03:21 -0600

    feat: Add styling to characters page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,11 +2,16 @@ import { Link } from "react-router-dom";
 import './LandingPage.css'
 
 function LandingPage() {
-  return(
-    <div className="button-container">
-      <Link to="/characters" className="characters-button-link">
-        <h1 className="characters-button" >View Characters</h1>
-      </Link>
+  return (
+    <div className="fullscreen-bg">
+      <img className="bg-vid" src="https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg" ></img>
+      <div className="content-container">
+        <div className="button-container">
+          <Link to="/characters" className="characters-button-link">
+            <h1 className="characters-button" >View Characters</h1>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
