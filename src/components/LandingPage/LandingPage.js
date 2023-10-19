1c30eb8e4dfc5afe9764580246785d5c4ed95c3d
Denzel Turambi <dturambi26@gmail.com>
2023-10-18 19:58:48 -0600

    refactor: Refactor Landing page styling

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,11 +3,11 @@ import './LandingPage.css'
 
 function LandingPage() {
   return(
-    <Link to="/characters" className="characters-button-link">
-      <div className="button-container">
+    <div className="button-container">
+        <Link to="/characters" className="characters-button-link">
         <h1 className="characters-button" >View Characters</h1>
-      </div>
     </Link>
+      </div>
   )
 }
 
