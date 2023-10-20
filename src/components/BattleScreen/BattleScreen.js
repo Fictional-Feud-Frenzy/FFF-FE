837c5143c929ca6040ab5a5b7a87635c860a32cf
Denzel Turambi <dturambi26@gmail.com>
2023-10-19 19:14:56 -0600

    feat: Style fight page

--- src/components/BattleScreen/BattleScreen.js
+++ src/components/BattleScreen/BattleScreen.js
@@ -4,19 +4,22 @@ import './BattleScreen.css';
 function BattleScreen({player1, player2, winner}){
   return(
     <div className="battle-screen">
-      <div className="battle-images"> 
+      <img className="bg-vid battle-bg-image" src="https://images.pexels.com/photos/2064827/pexels-photo-2064827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="arena"></img>
+      <div className="battle-images content-container"> 
         <div className="selected-fighter1">
           <img className="battle-image" src={player1.image} alt={player1.name} />
-          <p className="fighter1-name">{player1.name}</p>
+          <h1 className="fighter1-name">{player1.name}</h1>
         </div>
-        {winner?<p>{winner}</p>:<p className="bang">💥</p>}
+        <div className="fight-results">
+          {winner?<h2 className="declared-winner" >{winner}</h2>:<p className="bang">💥</p>}
+        </div>
         <div className="selected-fighter2">
           <img className="battle-image" src={player2.image} alt={player2.name} />
-          <p className="fighter2-name">{player2.name}</p>
+          <h1 className="fighter2-name">{player2.name}</h1>
         </div>
       </div>
-      <Link to="/characters">
-        <button>Return To All Characters</button>
+      <Link to="/characters" className="content-container">
+        <button className="return-button" >Return To All Characters</button>
       </Link>
     </div>
   )
