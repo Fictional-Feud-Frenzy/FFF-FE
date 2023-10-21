a2be330aa341f033df01d1fb7f233ada5c2a6658
Denzel Turambi <dturambi26@gmail.com>
2023-10-21 12:24:23 -0600

    feat: Add responsive design for CharacterInfo page

--- src/components/CharacterInfo/CharacterInfo.js
+++ src/components/CharacterInfo/CharacterInfo.js
@@ -1,7 +1,7 @@
 import './CharacterInfo.css';
 import { Link } from 'react-router-dom';
 import { useQuery, gql } from "@apollo/client"
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 const GET_CHARACTER = gql`
   query Character($id: ID!) {
@@ -31,56 +31,55 @@ const GET_CHARACTER = gql`
   }
 `;
 
-export default function CharacterInfo({setCharacter, character, selectPlayer1, selectPlayer2}){
+export default function CharacterInfo({ setCharacter, character, selectPlayer1, selectPlayer2 }) {
   let id = useParams().id
-  const {data, loading, error} = useQuery(GET_CHARACTER, {
+  const { data, loading, error } = useQuery(GET_CHARACTER, {
     variables: { id },
   });
   if (loading) return null;
   if (error) return `Error! ${error}`;
   setCharacter(data.character)
-return (
-  <div className="character-info">
-    <img className="bg-img bg-img-info" src="https://get.wallhere.com/photo/night-Batman-cave-midnight-comic-art-Bat-Cave-stage-theatre-darkness-screenshot-nightclub-music-venue-scenographer-195499.jpg" alt="batcave background"></img>
-    <div className="content-container">
-
-    <div className="character-header">
-      <Link to="/characters">
-        <button className="select-character-button" onClick={()=>selectPlayer1(character)}>select p1</button>
-      </Link>
-      <h1 className="white-text character-name">{character.name}</h1>
-      <Link to="/characters">
-        <button className="select-character-button" onClick={()=>selectPlayer2(character)}>select p2</button>
-      </Link>
-    </div>
-    <div className="description-container">
-      <div className="character-picpub" >
-        <img src={character.image} className="character-image" alt={character.name}/>
-        <p className="white-text">Published by: {character.publisher}</p>
+  return (
+    <div className="character-info">
+      <img className="bg-img bg-img-info" src="https://get.wallhere.com/photo/night-Batman-cave-midnight-comic-art-Bat-Cave-stage-theatre-darkness-screenshot-nightclub-music-venue-scenographer-195499.jpg" alt="batcave background"></img>
+      <div className="content-container">
+        <div className="character-header">
+          <Link to="/characters">
+            <button className="select-character-button" onClick={() => selectPlayer1(character)}>select p1</button>
+          </Link>
+          <h1 className="white-text character-name">{character.name}</h1>
+          <Link to="/characters">
+            <button className="select-character-button" onClick={() => selectPlayer2(character)}>select p2</button>
+          </Link>
+        </div>
+        <div className="description-container">
+          <div className="character-picpub" >
+            <img src={character.image} className="character-image" alt={character.name} />
+            <p className="white-text">Published by: {character.publisher}</p>
+          </div>
+          <div className="character-stats">
+            <p><strong>Name:</strong> {character.name} {character.fullName && `(${character.fullName})`}</p>
+            <p><strong>Good or Bad:</strong> {character.alignment}</p>
+            <p><strong>intelligence:</strong> {character.intelligence}</p>
+            <p><strong>Strength:</strong> {character.strength}</p>
+            <p><strong>Speed:</strong> {character.speed}</p>
+            <p><strong>Durability:</strong> {character.durability}</p>
+            <p><strong>Power:</strong> {character.power}</p>
+            <p><strong>Combat:</strong> {character.combat}</p>
+            <p><strong>Eye Color:</strong> {character.eyeColor}</p>
+            <p><strong>Gender:</strong> {character.gender}</p>
+            <p><strong>Team:</strong> {character.groupAffiliation}</p>
+            <p><strong>Hair Color:</strong> {character.hairColor}</p>
+            <p><strong>Height:</strong> {character.height}</p>
+            <p><strong>Weight:</strong> {character.weight}</p>
+            <p><strong>Place Of Birth:</strong> {character.placeOfBirth}</p>
+            <p><strong>Race:</strong> {character.race}</p>
+            <Link to="/characters" className="back-button">
+              <p> ⬅️ </p>
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="character-stats">
-        <p><strong>Name:</strong> {character.name} {character.fullName && `(${character.fullName})`}</p>
-        <p><strong>Good or Bad:</strong> {character.alignment}</p>
-        <p><strong>intelligence:</strong> {character.intelligence}</p>
-        <p><strong>Strength:</strong> {character.strength}</p>
-        <p><strong>Speed:</strong> {character.speed}</p>
-        <p><strong>Durability:</strong> {character.durability}</p>
-        <p><strong>Power:</strong> {character.power}</p>
-        <p><strong>Combat:</strong> {character.combat}</p>
-        <p><strong>Eye Color:</strong> {character.eyeColor}</p>
-        <p><strong>Gender:</strong> {character.gender}</p>
-        <p><strong>Team:</strong> {character.groupAffiliation}</p>
-        <p><strong>Hair Color:</strong> {character.hairColor}</p>
-        <p><strong>Height:</strong> {character.height}</p>
-        <p><strong>Weight:</strong> {character.weight}</p>
-        <p><strong>Place Of Birth:</strong> {character.placeOfBirth}</p>
-        <p><strong>Race:</strong> {character.race}</p>
-        <Link to="/characters" className="back-button">
-          <p> ⬅️ </p>
-        </Link>
-      </div>
     </div>
-    </div>
-  </div>
-)
+  )
 }
